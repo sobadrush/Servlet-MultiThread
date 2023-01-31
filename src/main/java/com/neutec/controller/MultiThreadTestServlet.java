90c32b70a66466cb2e7458de18480e0a5f55fd52
sobadrush <sobadrush@icloud.com>
2023-01-31 22:13:53 +0800

    format code

--- src/main/java/com/neutec/controller/MultiThreadTestServlet.java
+++ src/main/java/com/neutec/controller/MultiThreadTestServlet.java
@@ -1,10 +1,10 @@
 package com.neutec.controller;
 
-import sun.plugin2.message.Message;
-
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
